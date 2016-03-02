ceaa30e0b136de38a49f5712229a9bd862f8538f
Kartik Chaturvedi <kartikchaturvedi1@gmail.com>
2016-03-01 23:43:28 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,22 @@
+// var person = {
+//   name: 'Andrew',
+//   age: 21
+// }
+//
+// function updatePerson(obj) {
+//   obj.age = 24
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 88]
+
+function addGrades(gradesArr) {
+  gradesArr.push(55);
+  debugger;
+
+}
+
+addGrades(grades)
+console.log(grades);
