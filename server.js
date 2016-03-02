8e515ac5306b0732adff04b9204bd7b8c146dc4c
Kartik Chaturvedi <kartikchaturvedi1@gmail.com>
2016-03-01 19:06:07 -0500

    Add DELETE todos route

--- server.js
+++ server.js
@@ -28,6 +28,19 @@ app.get('/todos/:id', function (req, res) {
   }
 })
 
+app.delete('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    res.status(404).json({"error": "no todo found with that id"});
+  } else {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }
+
+})
+
 app.post('/todos', function (req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
@@ -36,7 +49,7 @@ app.post('/todos', function (req, res) {
   }
 
   body.description = body.description.trim();
-  
+
   body.id = todoNextId++
 
   todos.push(body);
